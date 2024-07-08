3e682faa218b3a99eb84430c239b18d0d545ff70
cdomet-d <coralie.dometdemont@gmail.com>
2024-07-08 18:32:42 +0200

    started on ex01

--- Contact.cpp
+++ Contact.cpp
@@ -1,34 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/02 15:44:58 by jauseff           #+#    #+#             */
-/*   Updated: 2024/07/08 14:52:00 by cdomet-d         ###   ########lyon.fr   */
+/*   Created: 2024/07/08 17:57:32 by cdomet-d          #+#    #+#             */
+/*   Updated: 2024/07/08 18:11:00 by cdomet-d         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Contact.hpp"
 #include <iostream>
-#include <cctype>
 
-int	main(int ac, char *av[])
+Contact::Contact()
 {
-
-	if (ac <= 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else
-	{
-		size_t	i = 1;
-		size_t	j = 0;
-		while (av[i])
-		{
-			j = 0;
-			while (av[i][j])
-				std::cout << static_cast<char>(std::toupper(av[i][j++]));
-			i++;
-		}
-	}
-	std::cout << std::endl;
+	std::cout << "constructor was called" << std::endl;
 }
+
--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -1,34 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/02 15:44:58 by jauseff           #+#    #+#             */
-/*   Updated: 2024/07/08 14:52:00 by cdomet-d         ###   ########lyon.fr   */
+/*   Created: 2024/07/08 14:24:52 by cdomet-d          #+#    #+#             */
+/*   Updated: 2024/07/08 18:15:04 by cdomet-d         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <cctype>
+#include <string>
 
-int	main(int ac, char *av[])
-{
+class Contact {
+	private:
+		std::string firstName;
+		std::string lastName;
+		std::string nickName;
+		std::string phoneNumber;
+		std::string darkestSecret;
 
-	if (ac <= 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else
-	{
-		size_t	i = 1;
-		size_t	j = 0;
-		while (av[i])
-		{
-			j = 0;
-			while (av[i][j])
-				std::cout << static_cast<char>(std::toupper(av[i][j++]));
-			i++;
-		}
-	}
-	std::cout << std::endl;
-}
+	public:
+		Contact();
+		void displayContact(void);
+};
+
