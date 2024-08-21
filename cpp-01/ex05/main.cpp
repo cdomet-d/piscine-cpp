e6804c3fb7f67a2f663d88b3ecfa50816c2a2794
cdomet-d <coralie.dometdemont@gmail.com>
2024-08-21 11:19:43 +0200

    add: cpp-01 to vogsphere

--- cpp-01/ex05/main.cpp
+++ cpp-01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 18:24:42 by cdomet-d          #+#    #+#             */
-/*   Updated: 2024/08/16 15:00:34 by cdomet-d         ###   ########.fr       */
+/*   Updated: 2024/08/21 11:08:57 by cdomet-d         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int main(int ac, char *argv[])
 	Harl test;
 
 	if (ac < 2)
-		return (std::cerr << "ERROR: No complaint level provided" << std::endl, 1);
+		return (std::cerr << "ERROR: No complain level provided" << std::endl, 1);
 	for (size_t i = 1; argv[i]; i++)
 	{
 		try {
