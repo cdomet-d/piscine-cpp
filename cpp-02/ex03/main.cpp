63c13b3dccf1930606e430c1a2af1c528f17f06a
cdomet-d <coralie.dometdemont@gmail.com>
2024-08-28 16:57:23 +0200

    add: geogebra file to test bsp

--- cpp-02/ex03/main.cpp
+++ cpp-02/ex03/main.cpp
@@ -6,17 +6,17 @@
 /*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 16:24:14 by cdomet-d          #+#    #+#             */
-/*   Updated: 2024/08/28 16:46:10 by cdomet-d         ###   ########lyon.fr   */
+/*   Updated: 2024/08/28 16:56:23 by cdomet-d         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
 int main(void) {
-	Point a(2.18, 1.63);
+	Point a(-2.18, -1.63);
 	Point b(4.5, 4);
 	Point c(10.68, 3.55);
-	Point point(10.6722226452848, 3.5474439594559);
+	Point point(-1, -0.8);
 	std::cout << point << std::endl;
 	try {
 		if (bsp(a, b, c, point) ==  true)
