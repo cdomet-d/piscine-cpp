9f2e85f8b3da110dbed0fe8869df635246fd7704
Jauppp <coralie.dometdemont@gmail.com>
2024-08-30 15:15:49 +0200

    add: finished bsp.md

--- cpp-02/ex03/bsp.cpp
+++ cpp-02/ex03/bsp.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   bsp.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: jauseff <jauseff@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 16:25:52 by cdomet-d          #+#    #+#             */
-/*   Updated: 2024/08/29 17:24:13 by cdomet-d         ###   ########lyon.fr   */
+/*   Updated: 2024/08/30 15:15:01 by jauseff          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,26 +25,27 @@ static bool triangleIsValid(Point const a, Point const b, Point const c)
 
 bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
-
 	Fixed w1;
 	Fixed w2;
+	Fixed n = 1;
 
 	std::cout << "point is interpreted as: " << point << std::endl;
 
 	if (triangleIsValid(a, b, c) == false)
 		return false;
-	Fixed vACY = c.getY() - a.getY();
-	Fixed vACX = c.getX() - a.getX();
 
+	Fixed vACX = c.getX() - a.getX();
+	Fixed vACY = c.getY() - a.getY();
 	Fixed vABY = b.getY() - a.getY();
 	Fixed vABX = b.getX() - a.getX();
+	Fixed vAPY = point.getY() - a.getY();
 
-	w1 = (a.getX() * vACY + (point.getY() - a.getY()) * vACX - point.getX() * vACY) / (vABY * vACX - vABX * vACY);
-	w2 = (point.getY() - a.getY() - w1 * vABY) / vACY;
-	std::cout << "w1: " << w1 << " | w2: " << w2 << " | w1 + w2: " << w1 + w2 << std::endl;
+	w1 = (a.getX() * vACY + vAPY * vACX - point.getX() * vACY) / (vABY * vACX - vABX * vACY);
+	w2 = (vAPY - w1 * vABY) / vACY;
+	Fixed w3 = n - w1 - w2;
+	std::cout << "w1: " << w1 << " | w2: " << w2 << " | w3: " << w3 << " | w1 + w2: " << w1 + w2 << std::endl;
 	if ((w1 > 0 && w1 < 1) && (w2 > 0 && w2 < 1) && (w2 + w1 <= 1))
 		return true;
 	else
 		return false;
 }
- 
