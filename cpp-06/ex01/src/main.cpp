d6d95c3c699ee0e2316526cb4ed87f1f085e520d
Coralie Domet De Mont <cdomet-d@z1r3p2.42lyon.fr>
2024-11-29 15:45:04 +0100

    chore: formatted main for clarity

--- cpp-06/ex01/src/main.cpp
+++ cpp-06/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 14:43:18 by cdomet-d          #+#    #+#             */
-/*   Updated: 2024/11/29 15:13:06 by cdomet-d         ###   ########lyon.fr   */
+/*   Updated: 2024/11/29 15:44:35 by cdomet-d         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,29 @@ int main(void)
 	Data square = {10, 10, 10, 0, 0};
 
 	std::cout << "Creating Data square = {10, 10, 10, 0, 0}:" << std::endl
-			  << square << std::endl << std::endl;
+			  << square << std::endl
+			  << std::endl;
 
 	uintptr_t sraw = 0;
-	std::cout << "Creating uintptr_t sraw: " << sraw << std::endl << std::endl;
+	std::cout << "Creating uintptr_t sraw: " << sraw
+			  << std::endl;
 	sraw = Serializer::serialize(&square);
-	std::cout << "After serializing square in sraw: " << sraw << std::endl << std::endl;
+	std::cout << "After serializing square in sraw: " << sraw << std::endl
+			  << std::endl;
 
-	Data *new_square = NULL;
-	std::cout << "Creating Data *new_square = NULL to get deserialized data from sraw: "
-			  << new_square << std::endl << std::endl;
-	
+	Data nsquare = {45, 45, 45, 20, 20};
+	Data *new_square = &nsquare;
+	std::cout << "Creating Data nsquare = {45, 45, 45, 20, 20}; \
+and Data *new_square = &nsquare;"
+			  << std::endl
+			  << "Printing *new_square"
+			  << std::endl
+			  << *new_square << std::endl
+			  << std::endl;
+
 	new_square = Serializer::deserialize(sraw);
 
-	std::cout << "Data *new_square after deserialization" << std::endl
+	std::cout << "Printing *new_square after deserialization" << std::endl
 			  << *new_square << std::endl;
 	return 0;
 }
