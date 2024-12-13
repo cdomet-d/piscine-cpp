d5df9ad113a3e49127182a4836db0a086c944090
Coralie Domet De Mont <cdomet-d@z1r3p2.42lyon.fr>
2024-12-13 14:58:17 +0100

    fix: minor formatting CPP-08/ex02

--- cpp-08/ex02/src/main.cpp
+++ cpp-08/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 12:51:48 by cdomet-d          #+#    #+#             */
-/*   Updated: 2024/12/12 17:29:56 by cdomet-d         ###   ########lyon.fr   */
+/*   Updated: 2024/12/13 14:57:47 by cdomet-d         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 #include <stack>
 #include <vector>
 
-#define TEST_MUTANT(Container, Type, SubContainer, Instance, Method)         \
+#define TEST_MUTANT(Container, Type, SubContainer, Instance, Method)           \
 	std::cout << std::endl                                                     \
 			  << "" #Container "<" #Type ", " #SubContainer "> > " #Instance   \
 				 ""                                                            \
@@ -41,11 +41,37 @@
 		std::cout << *it << std::endl;                                         \
 		++it;                                                                  \
 	}                                                                          \
-	std::stack< Type, SubContainer< Type > > s(mstack);
+	std::stack< Type, SubContainer< Type > > s(Instance);
 
 int main(void)
 {
 	{TEST_MUTANT(MutantStack, int, std::deque, mstack, push);}
 	{TEST_MUTANT(MutantStack, int, std::list, mstack, push);}
 	{TEST_MUTANT(MutantStack, int, std::vector, mstack, push);}
+
+	{
+		std::cout << std::endl
+				  << "Comparison with std::list<int>" << std::endl
+				  << std::endl;
+		std::list< int > nlist;
+		nlist.push_back(5);
+		nlist.push_back(17);
+		std::cout << nlist.back() << std::endl;
+		nlist.pop_back();
+		std::cout << nlist.size() << std::endl;
+		nlist.push_back(3);
+		nlist.push_back(5);
+		nlist.push_back(737);
+		nlist.push_back(0);
+		std::list< int >::iterator it = nlist.begin();
+		std::list< int >::iterator ite = nlist.end();
+		++it;
+		--it;
+		while (it != ite) {
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::list< int > s(nlist);
+		return 0;
+	}
 }
