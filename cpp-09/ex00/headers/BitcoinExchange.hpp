94b7cfc4557b7d0f08b26068f6fc73ad955f65be
Coralie Domet De Mont <cdomet-d@z1r3p2.42lyon.fr>
2024-12-13 17:02:11 +0100

    cpp-09/ex00
    - created directories and source files
    - created Makefile
    - imported database

---
+++ cpp-09/ex00/headers/BitcoinExchange.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 16:58:51 by cdomet-d          #+#    #+#             */
+/*   Updated: 2024/12/13 16:59:05 by cdomet-d         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+class BitcoinExchange {
+  public:
+	/*                               ORTHODOX CLASS                           */
+	BitcoinExchange(void);
+	BitcoinExchange(const BitcoinExchange &copy);
+	~BitcoinExchange(void);
+	BitcoinExchange &operator=(const BitcoinExchange &copy);
+
+	/*                               METHODS                                  */
+
+	/*                               GETTERS                                  */
+
+	/*                               SETTERS                                  */
+  private:
+};
+
+#endif
