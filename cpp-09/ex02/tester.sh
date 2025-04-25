62efa513468cd0a9364ef93575c3e709e912953a
Coralie Domet De Mont <cdomet-d@z1r3p2.42lyon.fr>
2025-04-25 11:43:27 +0200

    chore: added new test to BTC and updated script test for PmergeMe

--- cpp-09/ex02/tester.sh
+++ cpp-09/ex02/tester.sh
@@ -1,21 +1,18 @@
-#usr/bin/bash
-
+#!usr/bin/bash
 
 if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Usage: bash 1000tests.sh <iterations> <value range>"
-    exit 1
+	echo "Usage: bash 1000tests.sh <iterations> <value range>"
+	exit 1
 fi
 
 echo "Running $1 tests for $2 values"
 
 for ((i = 0; i < $1; i++)); do
-	./PmergeMe $(shuf -i 0-$2 -n $2 | tr "\n" " ") >> 1000test.log
+	./PmergeMe $(shuf -i 0-"$2" -n "$2" | tr "\n" " ") >> 1000test.log
 done
 
 echo "Total number of tests: $i"
 echo -n "Success: "
-cat 1000test.log | grep "successfully sorted" | wc -l 
+cat <1000test.log | grep -c "successfully sorted"
 echo -n "Failure: "
-cat 1000test.log | grep "not sorted" | wc -l
-
-rm 1000test.log
+cat <1000test.log | grep -c "not sorted"
